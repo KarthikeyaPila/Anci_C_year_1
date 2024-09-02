329144d027f391faa981f4ddd0ce965a88e45de2
KarthikeyaPila <karthikeyapila3@gmail.com>
2024-09-02 19:53:15 +0530

    updated.

--- Book_Problems.c
+++ Book_Problems.c
@@ -899,24 +899,31 @@ Display the contents of the matrix using not more than two printf statements.
 
 
 int main(){
-    int matrix[5][5];
+    char matrix[5][5];
     int i, j;
 
     for(i = 0; i < 5 ; i++){
         for(j = 0; j < 5 ; j++){
-            if(i+j == 4){matrix[i][j] = 0;}
-            else if(i + j > 4){matrix[i][j] = -1;}
-            else if(i + j < 4){matrix[i][j] = +1;}
+            if(i+j == 4){matrix[i][j] = "0";}
+            else if(i + j > 4){matrix[i][j] = "-1";}
+            else if(i + j < 4){matrix[i][j] = "+1";}
         }
     }
     for(i = 0; i < 5 ; i++){
         for(j = 0; j < 5 ; j++){
-            printf("%d " , matrix[i][j]);
+            printf("%c " , matrix[i][j]);
         }
     printf("\n");
     }
     return 0;
 }
 */
+
+
+//
+
+
+
+
 
 
