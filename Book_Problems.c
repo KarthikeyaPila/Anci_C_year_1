d6bf9abee9bdf50a9d221067af5a58ae3e3aa936
KarthikeyaPila <karthikeyapila3@gmail.com>
2024-09-03 20:32:36 +0530

    book problems Anci C, till arrays.

--- Book_Problems.c
+++ Book_Problems.c
@@ -950,7 +950,37 @@ int main()
 }
 */
 
+/* write a program that will compute the length of a given character string.
 
-//
+    int
+    main()
+{
+    char char_string[] = "hello there karthikeya";
+    int length = sizeof(char_string) / sizeof(char);
 
+    printf("the length of the given character string is: %d characters. \n", length - 1);
 
+    return 0;
+}
+*/
+
+/*write a program that will count the number of occurrences of a specified character in a given line of text.
+
+int main(){
+
+    char text[] = "hello there! 1 2 3 , my name is karthikeya, this is a test, zoop. \n";
+
+    int length = sizeof(text)/sizeof(char);
+    char character , occurance = 0;
+    printf("what character do you want to see the occurance for? \n");
+    scanf("%c" , &character);
+
+    for(int i = 0; i < length -1 ; i++){
+        if(text[i] == character){
+            occurance++;
+        }
+    }
+    printf("the character: %c has occured %d number of times.\n" , character , occurance);
+    return 0;
+}
+*/
