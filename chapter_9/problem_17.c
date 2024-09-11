0a5e9fadcbe4a50ff72be64bec8c56227957e242
Josh Kumar Polamarasetti <josh.kumar@ballys.com>
2024-09-11 18:27:26 +0100

    get small and large string length

--- chapter_9/problem_17.c
+++ chapter_9/problem_17.c
@@ -4,8 +4,27 @@
 int main() {
   const char* str = "this is going very long";
   int str_len = strlen(str);
+  int small_len = str_len;
+  int large_len = 0;
+  int cur_word_len = 0;
 
-  printf("%d\n", str_len);
+  for(int i=0; i<str_len; i++){
+    if(str[i] == ' '){
+      if(cur_word_len < small_len){
+        small_len = cur_word_len;
+      }
+      if(cur_word_len > large_len){
+        large_len = cur_word_len;
+      }
+
+      cur_word_len = 0;
+    } else {
+      cur_word_len++;
+    }
+  }
+
+  printf("small_len = %d\n", small_len);
+  printf("large_len = %d\n", large_len);
 
   return 0;
 }
