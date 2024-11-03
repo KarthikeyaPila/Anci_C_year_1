d6407dbbe282de074c42ff037cbb4c4f8a2d3367
KarthikeyaPila <karthikeyapila3@gmail.com>
2024-11-03 22:15:51 +0530

    [TO DO]

---
+++ 13.10.c
@@ -0,0 +1,5 @@
+// Write a program to read the file created in Exercise 13.8 to store the details in a random access file and print the detials of alternate
+// products from the file. Moidfy the program so that it can output the details of a product when its code is specified interactively.
+
+[TO DO]
+
